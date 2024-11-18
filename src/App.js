1a81205c85858e474078d644f8c98b8c9b35b56d
zeroweek <zeroweek4324@gmail.com>
2024-11-18 10:55:13 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import NavBar from './components/NavBar';
+import Splash from './pages/Splash';
+import MainPage from './pages/Mainpage';
+import CategoryPage from './pages/CategoryPage';
+import BudgetPredictionPage from './pages/BudgetPredictionPage';
+import SavingsCalculationPage from './pages/SavingsCalculationPage';
+import BucketListPage from './pages/BucketListPage';
+import RecommendationPage from './pages/RecommendationPage';
+
+function AppContent() {
+  const location = useLocation();
+  const isSpashPage = location.pathname === '/';
+
+  return (
+    <div style={{ position: 'relative', minHeight: '100vh' }}>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/category" element={<CategoryPage />} />
+        <Route path="/budget-prediction" element={<BudgetPredictionPage />} />
+        <Route path="/savings-calculation" element={<SavingsCalculationPage />} />
+        <Route path="/bucket-list" element={<BucketListPage />} />
+        <Route path="/recommendation" element={<RecommendationPage />} />
+      </Routes>
+      {!isSpashPage && <NavBar />}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppContent />
+    </Router>
   );
 }
 
---
+++ src/pages/SavingsCalculationPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SavingsCalculationPage() {
+  return (
+    <div>
+      <h1>저축 계산 페이지</h1>
+    </div>
+  );
+}
+
+export default SavingsCalculationPage;
